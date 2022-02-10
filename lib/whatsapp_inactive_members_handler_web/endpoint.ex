d54ff8eafd821d5c1909d7ed5e88d5e444467629
Jordan Hammond <jahammo2@gmail.com>
2022-02-10 15:00:52 -0500

    new tests

--- lib/whatsapp_inactive_members_handler_web/endpoint.ex
+++ lib/whatsapp_inactive_members_handler_web/endpoint.ex
@@ -10,7 +10,7 @@ defmodule WhatsappInactiveMembersHandlerWeb.Endpoint do
     signing_salt: "tU/Rm0Xa"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options], timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
